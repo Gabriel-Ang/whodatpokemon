2e84f5b1e97ed24de4a52d8160e319c1f5f6a92b
Gabriel <gawyyyyy@gmail.com>
2024-06-11 00:21:23 +0800

    added play model, controller and route

---
+++ app/backend/src/models/play.ts
@@ -0,0 +1,12 @@
+export class Play {
+    constructor(public id : string, public text : string){}
+}
+
+// export class Play {
+//     id : string;
+//     text : string;
+//     constructor(id : string, text : string){
+//         this.id = id;
+//         this.text = text;
+//     }
+// }

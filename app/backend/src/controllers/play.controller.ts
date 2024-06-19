948cdc048232d3964a9d1dd4a705a169272d7f2c
Gabriel <gawyyyyy@gmail.com>
2024-06-20 02:52:09 +0800

    Moved pokedex interface and constant, getRange() and getRandomUniqueNumbers() from index.ts to play.controller.ts

--- app/backend/src/controllers/play.controller.ts
+++ app/backend/src/controllers/play.controller.ts
@@ -1,20 +1,103 @@
 import express, { Express, Request, Response, NextFunction, RequestHandler } from "express";
 
 export const getPlay : RequestHandler = (req : Request, res : Response, next : NextFunction) => {
-    const text : any = req.body.text;
-    // const text = (req.body as { text:string } ).text;
-    res.status(200).json({
-        generation : [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    })
+    // const text : any = req.body.text;
+    // res.status(200).json({
+    //     generation : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // });
+    res.send('playpage');
 }
 
+interface PokedexGen {
+    gen : number;
+    index : [number, number]
+}
+
+const pokedex : PokedexGen[] = [
+    {
+        gen : 1,
+        index : [1, 151]
+    },
+    {
+        gen : 2,
+        index : [152, 251]
+    },
+    {
+        gen : 3,
+        index : [252, 386]
+    },
+    {
+        gen : 4,
+        index : [387, 493]
+    },
+    {
+        gen : 5,
+        index : [494, 649]
+    },
+    {
+        gen : 6,
+        index : [650, 721]
+    },
+    {
+        gen : 7,
+        index : [722, 809]
+    },
+    {
+        gen : 8,
+        index : [810, 905]
+    },
+    {
+        gen : 9,
+        index : [906, 1025]
+    },
+];
+
 export const createPlay : RequestHandler = (req : Request, res : Response, next : NextFunction) => {
-    const generation : any = req.body.generation;
-    console.log(generation);
-    
-    // const text = (req.body as { text:string } ).text;
+    const generation : number[] = req.body.generation;
+    const rounds : number = req.body.rounds;
+    console.log(req.body);
+    const selectedGens : PokedexGen[] = generation.map(num => pokedex[num - 1]);
+    const range : number[] = getRange(selectedGens);
+    const selectedPokemons : any = getRandomUniqueNumbers(rounds, range); // debug why I can't use static type, have to use any
+    console.log('selectedgens', selectedGens);
+    console.log('range', range);
+    console.log('selectedPokemons', selectedPokemons);
+    // const pokemonPromises : Promise<any>[] = [];
+    // for(let i = 0; i < rounds; i++){
+    // }
     res.status(201).json({
-        // message : text
-        'generation' : generation
-    })
+        'generation' : generation,
+        'rounds' : rounds
+    });
+}
+
+function getRange(data : PokedexGen[]) : number[] {
+    if(data.length === 0) {
+        throw new Error("No PokedexGen data provided.");
+    }
+    const range = data.flatMap(_data => {
+        const [start, end] = _data.index;
+        const numbersInRange = [];
+        for (let i = start; i <= end; i++) {
+            numbersInRange.push(i);
+        }
+        return numbersInRange;
+    });
+    range.sort((a, b) => a - b);
+    return range;
+}
+
+function getRandomUniqueNumbers(count : number, range : number[] ) {
+    if (count > range.length) {
+        throw new Error("Count exceeds the range of unique numbers available.");
+    }
+    const uniqueNumbers = new Set();
+    while (uniqueNumbers.size < count) {
+        const array = new Uint32Array(1);
+        crypto.getRandomValues(array);
+        const randomIndex = array[0] % range.length;
+        const randomNumber = range[randomIndex];
+        uniqueNumbers.add(randomNumber);
+    }
+    return Array.from(uniqueNumbers);
 }
--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -3,15 +3,17 @@ import dotenv from "dotenv";
 // import axios, { AxiosResponse, AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
 import playRoutes from './routes/play.route';
 import userRoutes from './routes/user.route';
+// import swaggerDocs from './utils/swagger'
 
 dotenv.config();
 
 const app : Express = express();
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 app.use(express.json());
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
+    // swaggerDocs(app, port);
 });
 
 // logger middleware
@@ -59,82 +61,3 @@ app.use('/user', userRoutes);
 //         res.status(500).send(err);
 //     }
 // });
-
-// interface PokedexGen {
-//     gen : number;
-//     index : [number, number]
-// }
-
-// const pokedex : PokedexGen[] = [
-//     {
-//         gen : 1,
-//         index : [1, 151]
-//     },
-//     {
-//         gen : 2,
-//         index : [152, 251]
-//     },
-//     {
-//         gen : 3,
-//         index : [252, 386]
-//     },
-//     {
-//         gen : 4,
-//         index : [387, 493]
-//     },
-//     {
-//         gen : 5,
-//         index : [494, 649]
-//     },
-//     {
-//         gen : 6,
-//         index : [650, 721]
-//     },
-//     {
-//         gen : 7,
-//         index : [722, 809]
-//     },
-//     {
-//         gen : 8,
-//         index : [810, 905]
-//     },
-//     {
-//         gen : 9,
-//         index : [906, 1025]
-//     },
-// ];
-
-// function getRange(data : PokedexGen[]) : [number, number] {
-//     if(data.length > 1){
-//         let res : number[] = [];
-//         // const indexes: number[] = data.flatMap((gen: PokedexGen) => gen.index);
-//         // const range: [number, number] = [Math.min(...indexes), Math.max(...indexes)];
-//         data.forEach((gen : PokedexGen) => {
-//             res = [...res, ...gen.index];
-//         });
-//         const range : [number, number] = [Math.min(...res), Math.max(...res)];
-//         return range;
-//     }else{
-//         return data[0].index;
-//     }
-// }
-
-// function getRandomUniqueNumbers(count : number, range : [number, number]) {
-//     const min : number = range[0];
-//     const max : number = range[1];
-//     if (count > (max - min + 1)) {
-//         throw new Error("Count exceeds the range of unique numbers available.");
-//     }
-
-//     const uniqueNumbers = new Set();
-
-//     while (uniqueNumbers.size < count) {
-//         const array = new Uint32Array(1);
-//         crypto.getRandomValues(array);
-//         const randomValue = array[0] / (0xFFFFFFFF + 1);
-//         const randomNumber = Math.floor(randomValue * (max - min + 1)) + min;
-//         uniqueNumbers.add(randomNumber);
-//     }
-
-//     return Array.from(uniqueNumbers);
-// }
